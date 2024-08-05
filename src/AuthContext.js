07856517b48a94527687a4758e2df9583f42a344
Ramagiri shiva shankara prasad <64155226+Shiva143-debug@users.noreply.github.com>
2024-08-05 13:13:42 +0530

    deleted comments in AuthContext.js

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -26,35 +26,3 @@ export const useAuth = () => {
     return useContext(AuthContext);
 };
 
-
-// import React, { createContext, useState, useContext } from 'react';
-
-// const AuthContext = createContext();
-
-// export const useAuth = () => useContext(AuthContext);
-
-// export const AuthProvider = ({ children }) => {
-//     const [auth, setAuth] = useState({ token: null, role: null });
-
-//     const login = async (email, password) => {
-//         // Assuming `loginApi` is your API call function
-//         const response = await login(email, password);
-//         const { token, role } = response.data;
-//         localStorage.setItem('token', token);
-//         localStorage.setItem('role', role);
-//         setAuth({ token, role });
-//     };
-
-//     const logout = () => {
-//         localStorage.removeItem('token');
-//         localStorage.removeItem('role');
-//         setAuth({ token: null, role: null });
-//     };
-
-//     return (
-//         <AuthContext.Provider value={{ auth, login, logout }}>
-//             {children}
-//         </AuthContext.Provider>
-//     );
-// };
-
